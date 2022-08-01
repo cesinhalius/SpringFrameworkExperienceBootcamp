035ca14e7fd84099da28a4d5b121059dc1fa559c
Cesar Augusto <cesinhalius@hotmail.com>
2022-08-01 16:03:08 -0300

    adicionando classe para testar o projeto

---
+++ Heranca/src/Exercicio/RodarAplicação.java
@@ -0,0 +1,15 @@
+package Exercicio;
+
+public class RodarAplicação {
+
+    public static void main(String[] args) {
+
+        Veiculo  automovel = new Veiculo();
+
+        Veiculo carro = new Carro();
+
+
+
+
+    }
+}
