cf4fa9e38eec1c5e27b4f79e9f5c88ae48b7583b
Cesar Augusto <cesinhalius@hotmail.com>
2022-08-08 16:02:21 -0300

    Adicionando a regra de negócios

--- Desafios/Abstraindo um Bootcamp Usando Orientação a Objetos em Java/desafio_POO/src/com/dio/desafio/Dev.java
+++ Desafios/Abstraindo um Bootcamp Usando Orientação a Objetos em Java/desafio_POO/src/com/dio/desafio/Dev.java
@@ -1,11 +1,31 @@
 package com.dio.desafio;
 
-import java.util.LinkedHashSet;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
+
 
 public class Dev {
+    public void InscreverBootcamp(Bootcamp bootcamp){
+        this.conteudoInscritos.addAll(bootcamp.getConteudo());
+        bootcamp.getDevsInscritos().add(this);
 
+    }
+
+    public void progredir(){
+
+        Optional<Conteudo> conteudo = this.conteudoInscritos.stream().findFirst();
+
+        if (conteudo.isPresent()){
+            this.conteudoConcluido.add(conteudo.get());
+            this.conteudoInscritos.remove((conteudo.get()));
+        }else {
+            System.err.println("Você nao esta matriculado em nenhum conteudo! ");
+        }
+    }
+
+
+    public double CalcularTotalXp(){
+        return this.conteudoConcluido.stream().mapToDouble(Conteudo:: calcularXp).sum();
+    }
     private String nome;
     private Set<Conteudo> conteudoInscritos= new LinkedHashSet<>();
     private  Set<Conteudo> conteudoConcluido = new LinkedHashSet<>();
@@ -41,18 +61,9 @@ public class Dev {
         Dev dev = (Dev) o;
         return nome.equals(dev.nome) && conteudoInscritos.equals(dev.conteudoInscritos) && conteudoConcluido.equals(dev.conteudoConcluido);
     }
-
     @Override
     public int hashCode() {
         return Objects.hash(nome, conteudoInscritos, conteudoConcluido);
     }
-
-    public void InscreverBootcamp(Bootcamp bootcamp){
-
-    }
-
-    public void progredir(){}
-
-
-    public void CalcularTotalXp(){}
 }
+
